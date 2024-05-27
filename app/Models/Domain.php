a6ffbb944d2e56ec08ba84cfd71f8b2775c99d02
sleeppygirls <lalalatte2344@gmail.com>
2024-05-27 09:49:00 +0700

    add Domain model

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -5,17 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Aspek extends Model
+class Domain extends Model
 {
     use HasFactory;
-
-    protected $table = 'aspeks';             // memanggil nama table
+    protected $table = 'domains';             // memanggil nama table
 
     protected $primaryKey = 'id';    // primary key ny
     protected $keyType = 'integer';              // merubah type data agar terpanggil
     protected $fillable = [                     // nama kolom yang dapat di edit
         'id',
-        'aspek',
+        'domain',
     ];
 
     // public function aspek() {
--- database/migrations/2024_05_27_024653_create_domains_table.php
+++ database/migrations/2024_05_27_024653_create_domains_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('aspeks', function (Blueprint $table) {
+        Schema::create('domains', function (Blueprint $table) {
             $table->id();
-            $table->string('aspek')->nullable();
+            $table->string('domain')->nullable();
             $table->timestamps();
         });
     }
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('aspeks');
+        Schema::dropIfExists('domains');
     }
 };

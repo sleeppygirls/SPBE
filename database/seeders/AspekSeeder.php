418099c8cfb2f7a9186af4bbc860e697e774823f
sleeppygirls <lalalatte2344@gmail.com>
2024-06-10 18:28:01 +0700

    revisi seeder

--- database/seeders/AspekSeeder.php
+++ database/seeders/AspekSeeder.php
@@ -16,27 +16,43 @@ class AspekSeeder extends Seeder
         $data = [
             [
                 'aspek' => 'Kebijakan Internal Tata Kelola SPBE',
+                'bobot_a' => 1.30,
+                'bobot_aspeka' => 100.00,
             ],
             [
                 'aspek' => 'Perencanaan Strategis SPBE',
+                'bobot_a' => 10.00,
+                'bobot_aspeka' => 40.00,
             ],
             [
                 'aspek' => 'Teknologi Informasi dan Komunikasi',
+                'bobot_a' => 10.00,
+                'bobot_aspeka' => 40.00,
             ],
             [
                 'aspek' => 'Penyelenggara SPBE',
+                'bobot_a' => 5.00,
+                'bobot_aspeka' => 20.00,
             ],
             [
                 'aspek' => 'Penerapan Manajemen SPBE',
+                'bobot_a' => 12.00,
+                'bobot_aspeka' => 72.70,
             ],
             [
                 'aspek' => 'Audit TIK',
+                'bobot_a' => 4.50,
+                'bobot_aspeka' => 27.30,
             ],
             [
                 'aspek' => 'Layanan Pemerintahan Berbasis Elektronik',
+                'bobot_a' => 27.50,
+                'bobot_aspeka' => 60.40,
             ],
             [
                 'aspek' => 'Layanan Publik Berbasis Elektronik',
+                'bobot_a' => 18.00,
+                'bobot_aspeka' => 39.60,
             ],
         ];
 
